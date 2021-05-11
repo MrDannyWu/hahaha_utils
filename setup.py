0b5dee5434db5b77a350bd29c8fc6a6d153daa24
danny <danny@kreditme.id>
2021-05-11 15:24:55 +0800

    1. update

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup_args = {
     'name': 'hahaha_utils',
     'author': 'Danny',
     'author_email': 'mrdanny1024@gmail.com',
-    'version': "1.0.5",
+    'version': "1.0.6",
     'license': 'MIT',
     'description': 'HAHAHA Python3 Utils',
     'long_description': 'HAHAHA Python3 Utils',
